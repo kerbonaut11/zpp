846dff14aef9f87a364dfa20895871c9d63dfb11
kerbonaut11 <tobiasfetzer09@gmx.de>
2026-02-02 20:57:52 +0100

    better ast dump

--- src/Ast.zig
+++ src/Ast.zig
@@ -80,66 +80,18 @@ extra: std.ArrayList(u32),
 errors: std.ArrayList(Error),
 
 
-inline fn tokenSrc(ast: *const Ast, token: Token.Idx) []const u8 {
+pub inline fn tokenSrc(ast: *const Ast, token: Token.Idx) []const u8 {
     const loc = ast.tokens.items(.loc)[token];
     return ast.src[loc.start..loc.end];
 }
 
 pub const parse = Parser.parse;
 
-
-pub fn dumpNode(ast: *const Ast, w: *std.io.Writer, node: Node.Idx, depth: u32) !void {
-    try w.splatByteAll(' ', depth*2);
-
-    switch (ast.nodes.get(node)) {
-        .int => |token| try w.print("{s}\n", .{ast.tokenSrc(token)}),
-
-        .unary_op => |op| {
-            try w.print("{s} {{\n", .{ast.tokenSrc(op.op_token)});
-            ast.dumpNode(w, op.val, depth+1) catch unreachable;
-            try w.splatByteAll(' ', depth*2);
-            try w.print("}}\n", .{});
-        },
-
-        .binary_op => |op| {
-            try w.print("{s} {{\n", .{ast.tokenSrc(op.op_token)});
-            try ast.dumpNode(w, op.lhs, depth+1);
-            try ast.dumpNode(w, op.rhs, depth+1);
-            try w.splatByteAll(' ', depth*2);
-            try w.print("}}\n", .{});
-        },
-
-        .block => |block| {
-            try w.print("{{\n", .{});
-            for (ast.extra.items[block.statement_idx..][0..block.statement_count]) |i| {
-                try ast.dumpNode(w, i, depth+1);
-            }
-            try w.splatByteAll(' ', depth*2);
-            try w.print("}}\n", .{});
-        },
-
-        .var_decl => |decl| {
-            try w.print("{s} {s} {{\n", .{ast.tokenSrc(decl.main_token), ast.tokenSrc(decl.main_token+1)});
-            if (decl.type != 0) try ast.dumpNode(w, decl.type, depth+1);
-            try ast.dumpNode(w, decl.val, depth+1);
-            try w.splatByteAll(' ', depth*2);
-            try w.print("}}\n", .{});
-        },
-
-        .fn_decl => |decl| {
-            _ = decl;
-            @panic("todo");
-        },
-
-        .invalid, .root => unreachable,
-    }
-}
-
-pub fn dump(ast: Ast) !void {
+pub fn dump(ast: *const Ast) !void {
     var buf: [64]u8 = undefined;
     const stdout = std.debug.lockStderrWriter(&buf);
     defer std.debug.unlockStdErr();
-    try ast.dumpNode(stdout, ast.nodes.get(0).root, 0);
+    try @import("dump_ast.zig").dumpNode(ast, stdout, ast.nodes.get(0).root, "root", 0);
     try stdout.flush();
 }
 
---
+++ src/dump_ast.zig
@@ -0,0 +1,56 @@
+const std = @import("std");
+const Ast = @import("Ast.zig");
+const Node = Ast.Node;
+
+pub fn printCloseBrace(w: *std.Io.Writer, depth: u32) !void {
+    try w.splatByteAll(' ', depth*2);
+    try w.print("}}\n", .{});
+}
+
+pub fn dumpNode(ast: *const Ast, w: *std.Io.Writer, node: Node.Idx, label: []const u8, depth: u32) !void {
+    try w.splatByteAll(' ', depth*2);
+    if (label.len != 0) {
+        try w.print("{s}: ", .{label});
+    }
+
+    switch (ast.nodes.get(node)) {
+        .int => |token| try w.print("{s}\n", .{ast.tokenSrc(token)}),
+
+        .unary_op => |op| {
+            try w.print("{s} {{\n", .{ast.tokenSrc(op.op_token)});
+            dumpNode(ast, w, op.val, "", depth+1) catch unreachable;
+            try printCloseBrace(w, depth);
+        },
+
+        .binary_op => |op| {
+            try w.print("{s} {{\n", .{ast.tokenSrc(op.op_token)});
+            try dumpNode(ast, w, op.lhs, "lhs", depth+1);
+            try dumpNode(ast, w, op.rhs, "rhs", depth+1);
+            try printCloseBrace(w, depth);
+        },
+
+        .block => |block| {
+            try w.print("{{\n", .{});
+            for (ast.extra.items[block.statement_idx..][0..block.statement_count], 0..) |idx, n| {
+                var buf: [8]u8 = undefined;
+                try dumpNode(ast, w, idx, try std.fmt.bufPrint(&buf, "{}", .{n}), depth+1);
+            }
+            try printCloseBrace(w, depth);
+        },
+
+        .var_decl => |decl| {
+            try w.print("{s} {s} {{\n", .{ast.tokenSrc(decl.main_token), ast.tokenSrc(decl.main_token+1)});
+            if (decl.type != 0) try dumpNode(ast, w, decl.type, "type", depth+1);
+            try dumpNode(ast, w, decl.val, "val", depth+1);
+            try w.splatByteAll(' ', depth*2);
+            try w.print("}}\n", .{});
+        },
+
+        .fn_decl => |decl| {
+            _ = decl;
+            @panic("todo");
+        },
+
+        .invalid, .root => unreachable,
+    }
+}
